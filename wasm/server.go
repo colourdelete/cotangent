36f88f2798494705f72c0961bc8ee860be1b2463
Ken Shibata <kenxshibata@gmail.com>
2022-01-06 11:48:59 -0500

    add a cool testing page

--- wasm/server.go
+++ wasm/server.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+	log.Println("will listen on :9090")
+	log.Println("only for testing")
 	err := http.ListenAndServe(":9090", http.FileServer(http.Dir(".")))
 	if err != nil {
 		fmt.Println("Failed to start server", err)
